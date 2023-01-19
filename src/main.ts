3d4f45045bd769b2abf74b272e7bfdeacb898488
Jérémy FONTAINE <jfontaine@groupeomicrone.com>
2023-01-19 20:12:00 +0000

    first commit

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+
+
+async function bootstrap() {
+	const app = await NestFactory.create(AppModule);
+	const configService = app.get(ConfigService);
+	const APP_PORT: number = configService.get<number>('APP_PORT');
+	await app.listen(APP_PORT);
+}
+bootstrap();
