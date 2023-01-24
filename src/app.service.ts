de78162498f13c948f85d0c7b3448287fd36a212
Jérémy FONTAINE <jfontaine@groupeomicrone.com>
2023-01-24 16:59:56 +0000

    identification machine

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import * as os from 'os';
 
 @Injectable()
 export class AppService {
@@ -7,6 +8,6 @@ export class AppService {
 	constructor(private configService: ConfigService) { }
 	getHello(ip: string): string {
 		const username: string = this.configService.get<string>('USERNAME') + ' ';
-		return `Hello ${username}from ${ip} !`;
+		return `Hello ${username}from ${os.hostname()} !`;
 	}
 }
