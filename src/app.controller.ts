a7a6d27ec315c89990e076480dfe31beab3e2790
Jeremy FONTAINE <jeremux.dev@gmail.com>
2023-03-29 22:11:41 +0200

    testing json return

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
 	constructor(private readonly appService: AppService) { }
 
 	@Get()
-	getHello(@Ip() ip): string {
+	getHello(@Ip() ip): any {
 		return this.appService.getHello(ip);
 	}
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -6,8 +6,11 @@ import * as os from 'os';
 export class AppService {
 
 	constructor(private configService: ConfigService) { }
-	getHello(ip: string): string {
+	getHello(ip: string): any {
 		const username: string = this.configService.get<string>('USERNAME') + ' ';
-		return `Hello ${username}from ${os.hostname()} !`;
+		return {
+			username,
+			ip,
+		};
 	}
 }
